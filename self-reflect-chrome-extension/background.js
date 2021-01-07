4912a7f24ca61466035e51cf093ac6b61b3b1d67
Thenuka Siva <38299804+thenukasiva@users.noreply.github.com>
2021-01-06 23:17:15 -0500

    Add files via upload

--- self-reflect-chrome-extension/background.js
+++ self-reflect-chrome-extension/background.js
@@ -1,1 +1,9 @@
-#this file is empty
+chrome.app.runtime.onLaunched.addListener(function() {
+    chrome.app.window.create('index.html', {
+      id: 'main',
+      bounds: { width: 620, height: 500 }
+    });
+  });
+  chrome.alarms.onAlarm.addListener(function( alarm ) {
+    console.log("Got an alarm!", alarm);
+  });
